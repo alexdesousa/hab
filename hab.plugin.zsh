13b716103ebd95430a49901cdcffce3a543f1d7d
Alexander de Sousa <alexander.jose.de.sousa@gmail.com>
2019-10-30 19:27:46 +0100

    Added plugin update functions

--- hab.plugin.zsh
+++ hab.plugin.zsh
@@ -13,32 +13,32 @@
 export CURRENT_HAB=""
 [ -z "$HAB_BASE" ] && export HAB_BASE=".envrc"
 
-###########
-# Functions
+###################
+# Private functions
 
 # Prints an error
-function hab_error() {
+function __hab_error() {
   local message="$1"
 
   (>&2 echo -e "\033[1;91m[ERROR]    $message\033[0;0m")
 }
 
 # Prints a warning
-function hab_warn() {
+function __hab_warn() {
   local message="$1"
 
   (>&2 echo -e "\033[1;93m[WARN]     $message\033[0;0m")
 }
 
 # Prints a success message
-function hab_success() {
+function __hab_success() {
   local message="$1"
 
   (echo -e "\033[1;92m[SUCCESS]  $message\033[0;0m")
 }
 
 # Gets the name of the env file
-function get_hab_file() {
+function __hab_get_file() {
   local hab_type="$1"
   local current_path=""
   local hab_file=""
@@ -50,8 +50,8 @@ function get_hab_file() {
     hab_file="$current_path/$HAB_BASE.$hab_type"
   elif [ -n "$hab_type" ] && [ -r "$current_path/$HAB_BASE" ]
   then
-    hab_error "File not found [$current_path/$HAB_BASE.$hab_type]"
-    hab_warn "Using default instead [$current_path/$HAB_BASE]"
+    __hab_error "File not found [$current_path/$HAB_BASE.$hab_type]"
+    __hab_warn "Using default instead [$current_path/$HAB_BASE]"
     hab_file="$current_path/$HAB_BASE"
   elif [ -r "$current_path/$HAB_BASE" ]
   then
@@ -62,7 +62,7 @@ function get_hab_file() {
 }
 
 # Unloads variables
-function unload_variables() {
+function __hab_unload_variables() {
   local current_hab="$1"
   local unload=""
 
@@ -75,12 +75,12 @@ function unload_variables() {
   while read -r line
   do
     unset "$line"
-    hab_warn "Unloaded variable $line"
+    __hab_warn "Unloaded variable $line"
   done <<<"$(echo "$unload")"
 }
 
 # Unloads functions
-function unload_functions() {
+function __hab_unload_functions() {
   local current_hab="$1"
   local unload=""
 
@@ -93,42 +93,61 @@ function unload_functions() {
   while read -r line
   do
     unset -f "$line"
-    hab_warn "Unloaded function $line"
+    __hab_warn "Unloaded function $line"
   done <<<"$(echo "$unload")"
 }
 
 # Unloads current hab
-function unload_current_hab() {
+function __hab_unload_current() {
   local unload=""
 
   if [ -n "$CURRENT_HAB" ] && [ -r "$CURRENT_HAB" ]
   then
-    unload_variables "$CURRENT_HAB"
-    unload_functions "$CURRENT_HAB"
+    __hab_unload_variables "$CURRENT_HAB"
+    __hab_unload_functions "$CURRENT_HAB"
   fi
 
   export CURRENT_HAB=""
 }
 
 # Loads new hab
-function load_new_hab() {
+function __hab_load_new() {
   export CURRENT_HAB="$1"
   source "$CURRENT_HAB"
-  hab_success "Loaded hab [$CURRENT_HAB]"
+  __hab_success "Loaded hab [$CURRENT_HAB]"
 }
 
-# Loads a new
-function load_hab() {
+##################
+# Public functions
+
+# Updates hab plugin
+function hab_update() {
+  if [ -d "$ZSH_CUSTOM/plugins/hab/.git" ]
+  then
+    (
+      cd "$ZSH_CUSTOM/plugins/hab" &&
+      git pull origin master
+    )
+  fi
+}
+
+# Unloads current hab
+function hab_unload() {
+  __hab_unload_current
+}
+
+# Loads a new hab
+function hab_load() {
   local hab_type="$1"
   local hab_file=""
 
-  hab_file=$(get_hab_file "$hab_type")
+  hab_file=$(__hab_get_file "$hab_type")
 
-  unload_current_hab
+  hab_unload
 
   if [ -n "$hab_file" ]
   then
-    load_new_hab "$hab_file"
+    __hab_load_new "$hab_file"
   fi
 }
 
@@ -136,7 +155,7 @@ function load_hab() {
 # Installation
 
 # Loads hab on `cd`
-chpwd_functions=(${chpwd_functions[@]} "load_hab")
+chpwd_functions=(${chpwd_functions[@]} "hab_load")
 
 # Loads hab when openning a TMUX pane.
-[ -n "$TMUX" ] && load_hab
+[ -n "$TMUX" ] && hab_load
